08fd7a4081a3651e6374c1eab059f104febe6e94
fearclear <fearcleari@gmail.com>
2020-07-13 16:05:06 +0800

    remove test console

--- src/packages/tree-select/index.vue
+++ src/packages/tree-select/index.vue
@@ -141,7 +141,6 @@ export default class TreeSelect extends TreeSelectProps {
   }
 
   private visibleChange (visible: boolean) {
-    console.log('test console', visible)
     this.popoverVisible = visible
     if (visible) {
       this.$refs.tree.filter('')
